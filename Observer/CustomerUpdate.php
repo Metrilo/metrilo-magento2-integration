3f25b97082cdd159b1805ca099545c3737728691
Miroslav <miroslav.petrov@blue-leaf.co.uk>
2016-11-19 14:00:04 +0200

    add event for customer update and trigger identify method in metrilo

---
+++ Observer/CustomerUpdate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Metrilo\Analytics\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class CustomerUpdate implements ObserverInterface {
+
+    /**
+     * @param \Metrilo\Analytics\Helper\Data $helper
+     */
+    public function __construct(
+        \Metrilo\Analytics\Helper\Data $helper,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
+    ) {
+        $this->helper = $helper;
+        $this->customerRepository = $customerRepository;
+    }
+
+    /**
+     * Collect orders details
+     *
+     * @param  \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer) {
+        $email = $observer->getEvent()->getEmail();
+        if (empty($email) || !$email) {
+            return;
+        }
+
+        $customer = $this->customerRepository->get($email);
+        if ($customer) {
+            $data = [
+                'id' => $customer->getEmail(),
+                'params' => [
+                    'email'         => $customer->getEmail(),
+                    'name'          => $customer->getFirstname() .' '.$customer->getLastname(),
+                    'first_name'    => $customer->getFirstname(),
+                    'last_name'     => $customer->getLastname(),
+                ]
+            ];
+            $this->helper->addSessionEvent('identify', 'identify', $data);
+        }
+    }
+}
