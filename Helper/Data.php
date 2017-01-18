777ba2cc3bae26ca5bdcd365c6b320c536489078
Miroslav <miroslav.petrov@blue-leaf.co.uk>
2017-01-18 22:21:53 +0200

    add event for order update and send data to metrilo with async helper

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,7 +7,8 @@ namespace Metrilo\Analytics\Helper;
  *
  * @author Miroslav Petrov <miro91tn@gmail.com>
  */
-class Data extends \Magento\Framework\App\Helper\AbstractHelper {
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
 
     const DATA_TAG = "metrilo_events";
 
@@ -24,13 +25,17 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
         \Magento\Customer\Model\Session $session,
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\ObjectManagerInterface $objectManager,
-        \Magento\Catalog\Model\ProductRepository $productRepository
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        Async $asyncHelper
     ) {
         $this->config = $scopeConfig;
         $this->session = $session;
         $this->logger = $logger;
         $this->_objectManager = $objectManager;
         $this->productRepository = $productRepository;
+        $this->jsonHelper = $jsonHelper;
+        $this->asyncHelper = $asyncHelper;
     }
 
     /**
@@ -38,7 +43,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      *
      * @return boolean
      */
-    public function isEnabled() {
+    public function isEnabled()
+    {
         return $this->config->getValue('analytics/general/enable',
                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                );
@@ -49,7 +55,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      *
      * @return string
      */
-    public function getApiToken() {
+    public function getApiToken()
+    {
         return $this->config->getValue('analytics/general/api_key',
                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                );
@@ -60,7 +67,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      *
      * @return string
      */
-    public function getApiSecret() {
+    public function getApiSecret()
+    {
         return $this->config->getValue('analytics/general/api_secret',
                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                );
@@ -71,7 +79,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      *
      * @return array
      */
-    public function getSessionEvents() {
+    public function getSessionEvents()
+    {
         $events = [];
         if($this->session->getData(self::DATA_TAG)) {
             $events = $this->session->getData(self::DATA_TAG, true);
@@ -87,7 +96,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      * @param array   $data
      * @param boolean|string $metaData
      */
-    public function addSessionEvent($method, $type, $data, $metaData = false) {
+    public function addSessionEvent($method, $type, $data, $metaData = false)
+    {
         $events = [];
         if ($this->session->getData(self::DATA_TAG) != '') {
             $events = (array)$this->session->getData(self::DATA_TAG);
@@ -110,7 +120,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      * @param  \Exception $exception
      * @return void
      */
-    public function logError($exception) {
+    public function logError($exception)
+    {
         if ($exception instanceof \Exception) {
             $this->logger->critical($exception->getMessage());
         } else {
@@ -126,17 +137,20 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      */
     public function prepareOrderDetails($order)
     {
-        $data = array(
+        $data = [
             'order_id'          => $order->getIncrementId(),
             'cart_id'           => $order->getQuoteId(),
             'order_status'      => $order->getStatus(),
             'amount'            => (float)$order->getGrandTotal(),
             'shipping_amount'   => (float)$order->getShippingAmount(),
             'tax_amount'        => $order->getTaxAmount(),
-            'items'             => array(),
+            'items'             => [],
             'shipping_method'   => $order->getShippingDescription(),
             'payment_method'    => $order->getPayment()->getMethodInstance()->getTitle(),
-        );
+        ];
+
+        $this->_assignBillingInfo($data, $order);
+
         if ($order->getCouponCode()) {
             $data['coupons'] = array($order->getCouponCode());
         }
@@ -165,76 +179,138 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
     }
 
     /**
-     * Create HTTP request to metrilo server
+     * API call to Metrilo to submit information asynchronously
      *
-     * @param  string  $ident
-     * @param  string  $event
-     * @param  array  $params
-     * @param  boolean|array $identityData
-     * @param  boolean|int $time
-     * @param  boolean|array $callParameters
+     * @param  int $storeId
+     * @param  array $orders
      * @return void
      */
-    public function callApi($ident, $event, $params, $identityData = false, $time = false, $callParameters = false)
+    public function callBatchApi($storeId, $orders)
     {
         try {
-            $call = $this->buildEventArray($ident, $event, $params, $identityData, $time, $callParameters);
-            // We should handle the setting of token parameter, as it's part of the request
-            $call['token'] = $this->getApiToken();
-            // Additional ksort here because of adding token param
-            ksort($call);
-            $basedCall = base64_encode(Mage::helper('core')->jsonEncode($call));
-            $signature = md5($basedCall.$this->getApiSecret());
-            // Use Varien_Http_Client
-            // to generate API call end point and call it
-            $url = 'http://p.metrilo.com/t?s='.$signature.'&hs='.$basedCall;
-            $client = new Varien_Http_Client($url);
-            $response = $client->request();
-            $result = Mage::helper('core')->jsonDecode($response->getBody());
-            if (!$result['status']) {
-                Mage::log($result['error'], null, 'Metrilo_Analytics.log');
-            }
-        } catch (Exception $e){
-            Mage::log($e->getMessage(), null, 'Metrilo_Analytics.log');
+            $ordersForSubmition = $this->_buildOrdersForSubmition($orders);
+            $call = $this->_buildCall($storeId, $ordersForSubmition);
+            $this->_callMetriloApiAsync($storeId, $call);
+        } catch (Exception $e) {
+            $this->logError($e);
         }
     }
 
     /**
-     * [callBatchApi description]
-     * @param  [type] $ordersForSubmition [description]
-     * @return [type]                     [description]
+     * Submit orders to Metrilo API via post request
+     *
+     * @param  int $storeId
+     * @param  array $call
+     * @return void
      */
-    public function callBatchApi($ordersForSubmition)
+    private function _callMetriloApiAsync($storeId, $call)
     {
-        try {
-            // Consider token is in the first level in the hashed json
-            $version = (string)Mage::getConfig()->getModuleConfig("Metrilo_Analytics")->version;
-            $call = array(
-                'token'    => $this->getApiToken(),
-                'events'   => $ordersForSubmition,
-                // for debugging/support purposes
-                'platform' => 'Magento ' . Mage::getEdition() . ' ' . Mage::getVersion(),
-                'version'  => $version
-            );
-            // Additional ksort here because of adding token param
-            ksort($call);
-            $basedCall = base64_encode(Mage::helper('core')->jsonEncode($call));
-            $signature = md5($basedCall.$this->getApiSecret());
-            $url = 'http://p.metrilo.com/bt';
-            $client = new Varien_Http_Client($url);
-            $requestBody = array(
-                's'   => $signature,
-                'hs'  => $basedCall
-            );
-            // This method supports passing named array as well as key, value
-            $client->setParameterPost($requestBody);
-            $response = $client->request('POST');
-            if ($response->isError()) {
-                Mage::log($response->getBody(), null, 'Metrilo_Analytics.log');
+        ksort($call);
+        $basedCall = base64_encode($this->jsonHelper->jsonEncode($call));
+        $signature = md5($basedCall.$this->getApiSecret($storeId));
+        $requestBody = array(
+            's'   => $signature,
+            'hs'  => $basedCall
+        );
+        $this->asyncHelper->post('http://p.metrilo.com/bt', $requestBody);
+    }
+
+    /**
+     * Create submition ready arrays from Array of \Magento\Sales\Model\Order
+     *
+     * @param \Magento\Sales\Model\Order[] $orders
+     * @return Array
+     */
+    private function _buildOrdersForSubmition($orders)
+    {
+        $ordersForSubmition = array();
+        foreach ($orders as $order) {
+            if ($order->getId()) {
+                array_push($ordersForSubmition, $this->_buildOrderForSubmition($order));
             }
-        } catch (Exception $e) {
-            Mage::log($e->getMessage(), null, 'Metrilo_Analytics.log');
         }
+        return $ordersForSubmition;
+    }
+
+    /**
+     * Build individual order data
+     *
+     * @param  \Magento\Sales\Model\Order $order
+     * @return array
+     */
+    private function _buildOrderForSubmition($order)
+    {
+        $orderDetails = $this->prepareOrderDetails($order);
+        // initialize additional params
+        $callParameters = false;
+        // check if order has customer IP in it
+        $ip = $order->getRemoteIp();
+        if ($ip) {
+            $callParameters = array('use_ip' => $ip);
+        }
+        // initialize time
+        $time = false;
+        if ($order->getCreatedAt()) {
+            $time = $order->getCreatedAt();
+        }
+        $identityData = $this->_orderIdentityData($order);
+        return $this->_buildEventArray(
+            $identityData['email'], 'order', $orderDetails, $identityData, $time, $callParameters
+        );
+    }
+
+    /**
+     * Get Order Customer identity data
+     *
+     * @param  \Magento\Sales\Model\Order $order
+     * @return array
+     */
+    private function _orderIdentityData($order)
+    {
+        return array(
+            'email'         => $order->getCustomerEmail(),
+            'first_name'    => $order->getBillingAddress()->getFirstname(),
+            'last_name'     => $order->getBillingAddress()->getLastname(),
+            'name'          => $order->getBillingAddress()->getName(),
+        );
+    }
+
+    /**
+     * Create call array
+     *
+     * @param  int $storeId
+     * @param  array $ordersForSubmition
+     * @return array
+     */
+    private function _buildCall($storeId, $ordersForSubmition)
+    {
+        return array(
+            'token'    => $this->getApiToken($storeId),
+            'events'   => $ordersForSubmition,
+            // for debugging/support purposes
+            // 'platform' => 'Magento ' . Mage::getEdition() . ' ' . Mage::getVersion(),
+            // 'version'  => (string)Mage::getConfig()->getModuleConfig("Metrilo_Analytics")->version
+        );
+    }
+
+    /**
+     * Assign billing information
+     *
+     * @param  array $data
+     * @param  \Magento\Sales\Model\Order $order
+     * @return void
+     */
+    private function _assignBillingInfo(&$data, $order)
+    {
+        $billingAddress = $order->getBillingAddress();
+        // Assign billing data to order data array
+        $data['billing_phone']    = $billingAddress->getTelephone();
+        $data['billing_country']  = $billingAddress->getCountryId();
+        $data['billing_region']   = $billingAddress->getRegion();
+        $data['billing_city']     = $billingAddress->getCity();
+        $data['billing_postcode'] = $billingAddress->getPostcode();
+        $data['billing_address']  = $billingAddress->getStreetFull();
+        $data['billing_company']  = $billingAddress->getCompany();
     }
 
     /**
@@ -248,28 +324,26 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper {
      * @param  boolean|array $callParameters
      * @return void
      */
-    public function buildEventArray($ident, $event, $params, $identityData = false, $time = false, $callParameters = false)
+    public function _buildEventArray($ident, $event, $params, $identityData = false, $time = false, $callParameters = false)
     {
-      $call = array(
-          'event_type'    => $event,
-          'params'        => $params,
-          'uid'           => $ident
-      );
-      if($time) {
-          $call['time'] = $time;
-      }
-      // check for special parameters to include in the API call
-      if($callParameters) {
-          if($callParameters['use_ip']) {
-              $call['use_ip'] = $callParameters['use_ip'];
-          }
-      }
-      // put identity data in call if available
-      if($identityData) {
-          $call['identity'] = $identityData;
-      }
-      // Prepare keys is alphabetical order
-      ksort($call);
-      return $call;
+        $call = array(
+            'event_type'    => $event,
+            'params'        => $params,
+            'uid'           => $ident
+        );
+        if($time) {
+            $call['time'] = $time;
+        }
+        // check for special parameters to include in the API call
+        if($callParameters && $callParameters['use_ip']) {
+            $call['use_ip'] = $callParameters['use_ip'];
+        }
+        // put identity data in call if available
+        if($identityData) {
+            $call['identity'] = $identityData;
+        }
+        // Prepare keys is alphabetical order
+        ksort($call);
+        return $call;
     }
 }
