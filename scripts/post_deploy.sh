191b564429b371f53e8d401ecae857116beea014
Zhivko Draganov <zhivko.draganov@gmail.com>
2017-08-03 16:41:47 +0300

    Try in developer mode with static content deploy

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -11,8 +11,8 @@ su -c "bin/magento module:enable --all --clear-static-content" www-data
 
 # Flush the cache
 su -c "bin/magento cache:flush" www-data
-
-# su -c "bin/magento setup:di:compile" www-data
+su -c "bin/magento deploy:mode:set developer" www-data
+su -c "bin/magento setup:static-content:deploy" www-data
 su -c "bin/magento indexer:reindex" www-data
 
 su -c /sbin/my_init
