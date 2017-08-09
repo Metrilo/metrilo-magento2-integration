87ba96d4bf6ab2a7c6f642f97917af7ea15f0965
Zhivko Draganov <zhivko.draganov@gmail.com>
2017-08-09 16:07:22 +0300

    Remove old compilation directory

--- scripts/update_plugin.sh
+++ scripts/update_plugin.sh
@@ -8,6 +8,9 @@ composer require metrilo/analytics-magento2-extension:master@dev --no-update
 composer update
 
 bin/magento setup:upgrade
+
+rm -rf /magento2/var/di
+
 bin/magento setup:di:compile
 bin/magento setup:static-content:deploy
 bin/magento indexer:reindex
