994d9c32072f310c61d1093154624facdb9512f5
Ned <ned@metrilo.com>
2018-09-14 16:11:33 +0300

    PLG-36: fixed customer subscribed status data export

--- Model/CustomerData.php
+++ Model/CustomerData.php
@@ -18,19 +18,17 @@ class CustomerData
         $customers = $this->customerCollection->create()->addAttributeToFilter('store_id', $storeId);
 
         foreach ($customers as $customer) {
-            $customer = $customer->toArray();
-            $subscriberStatus = $this->subscriber->loadByCustomerId($customer['entity_id']);
-
-            $customersArray[$customer['entity_id']] = [
-                'email'     => $customer['email'],
-                'createdAt' => strtotime($customer['created_at']),
-                'updatedAt' => strtotime($customer['updated_at']),
-                'firstName' => $customer['firstname'],
-                'lastName'  => $customer['lastname'],
-                'subscribed'=> $subscriberStatus->isSubscribed()
+            $subscriberStatus = $this->subscriber->loadByEmail($customer['email'])->isSubscribed();
+            $this->subscriber->unsetData();
+            $customersArray[] = [
+                'email'     => $customer->getEmail(),
+                'createdAt' => strtotime($customer->getCreatedAt()),
+                'updatedAt' => strtotime($customer->getUpdatedAt()),
+                'firstName' => $customer->getFirstname(),
+                'lastName'  => $customer->getLastname(),
+                'subscribed'=> $subscriberStatus
             ];
         }
         return $customersArray;
     }
-
 }
