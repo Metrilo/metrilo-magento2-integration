7a0232c0cf090434837c6f46d9f6a6e12aa84066
Ned <ned@metrilo.com>
2018-09-26 17:52:22 +0300

    PLG-39: code refactoring

--- Model/ProductData.php
+++ Model/ProductData.php
@@ -15,56 +15,52 @@ class ProductData
     public function getProducts($storeId)
     {
         $productsArray = [];
-        $products = $this->productCollection->create()->addAttributeToSelect('*')->addStoreFilter($storeId);
+        $productOptions = '';
+        $products = $this->productCollection->create()->addAttributeToSelect('*')->addUrlRewrite()->addStoreFilter($storeId);
 
         foreach ($products as $product) {
-            $productOptions = [];
+            if ($product->getTypeId() == 'configurable' || $product->getTypeId() == 'bundle' || $product->getTypeId() == 'grouped') {
+                $productOptions = $this->getProductOptions($product, $storeId); // get product opitons for configurable/bundle/grouped product
+            }
 
             if($product->isVisibleInSiteVisibility()) { // CHECK FOR PRODUCT CATALOG VISIBILITY
-                $productOptions  = $this->getProductOptions($product); // get product opitons for configurable/bundle/grouped product
                 $productsArray[] = [
                     'categories' => $product->getCategoryIds(),
                     'id'         => $product->getId(),
                     'sku'        => $product->getSku(),
-                    'imageUrl'   => $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage(),
+                    'imageUrl'   => (!empty($product->getImage())) ? $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage() : '',
                     'name'       => $product->getName(),
-                    'price'      => ($product->getTypeId() == 'configurable' || 'bundle') ? number_format($product->getFinalPrice(), 4) : $product->getPrice(), // Does not return grouped/bundled parent price
-                    'url'        => $product->getProductUrl(),
+                    'price'      => (!empty($product->getPrice())) ? $product->getPrice() : 0, // Does not return grouped/bundled parent price
+                    'url'        => $this->storeManager->getStore($storeId)->getBaseUrl() . $product->getRequestPath(),
                     'options'    => $productOptions
                 ];
-            }     
+            }
         }
         
         return $productsArray;
     }
 
-    protected function getProductOptions($product)
+    protected function getProductOptions($product, $storeId)
     {
         $productOptions = [];
+        $childrenProducts = '';
 
         if ($product->getTypeId() == 'configurable') {
             $childrenProducts = $product->getTypeInstance()->getUsedProducts($product);
-            $productOptions   = $this->getChildProducts($childrenProducts);
         } elseif ($product->getTypeId() == 'bundle') {
             $childrenProducts = $product->getTypeInstance()->getSelectionsCollection($product->getTypeInstance()->getOptionsIds($product), $product);
-            $productOptions   = $this->getChildProducts($childrenProducts);
         } elseif ($product->getTypeId() == 'grouped') {
             $childrenProducts = $product->getTypeInstance()->getAssociatedProductCollection($product)->addAttributeToSelect(['name', 'price', 'image']);
-            $productOptions   = $this->getChildProducts($childrenProducts);
         }
 
-        return $productOptions;
-    }
-
-    protected function getChildProducts($childrenProducts) {
         foreach ($childrenProducts as $childProduct) {
             $productOptions[] = [
-                    'productId' => $childProduct->getId(),
-                    'sku'       => $childProduct->getSku(),
-                    'name'      => $childProduct->getName(),
-                    'price'     => $childProduct->getPrice(),
-                    'imageUrl'  => $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $childProduct->getImage()
-                ];
+                'productId' => $childProduct->getId(),
+                'sku'       => $childProduct->getSku(),
+                'name'      => $childProduct->getName(),
+                'price'     => $childProduct->getPrice(),
+                'imageUrl'  => $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $childProduct->getImage()
+            ];
         }
 
         return $productOptions;
