bd6d1e8e89c136821cbf6b639f92d182e858b553
Ned <ned@metrilo.com>
2018-10-10 15:22:25 +0300

    PLG-39: code readability improvement

--- Model/ProductData.php
+++ Model/ProductData.php
@@ -4,54 +4,52 @@ namespace Metrilo\Analytics\Model;
 
 class ProductData
 {
+    /**
+     * Product parent types
+     */
+    const PARENT_TYPES = [\Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE, \Magento\Bundle\Model\Product\Type::TYPE_CODE, \Magento\GroupedProduct\Model\Product\Type\Grouped::TYPE_CODE];
+
     public function __construct(
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollection,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Bundle\Model\Product\Type $bundleType,
         \Magento\GroupedProduct\Model\Product\Type\Grouped $groupedType,
-        \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable $configurableType
-
-
-    ) {
-        $this->productCollection     = $productCollection;
-        $this->storeManager          = $storeManager;
-        $this->bundleType            = $bundleType;
-        $this->groupedType           = $groupedType;
-        $this->configurableType      = $configurableType;
+        \Magento\ConfigurableProduct\Model\Product\Type\Configurable $configurableType
+    )
+    {
+        $this->productCollection = $productCollection;
+        $this->storeManager = $storeManager;
+        $this->bundleType = $bundleType;
+        $this->groupedType = $groupedType;
+        $this->configurableType = $configurableType;
     }
 
     public function getProducts($storeId)
     {
-        $productTypesWithOption = ['configurable', 'bundle', 'grouped'];
         $products = $this->productCollection->create()->addAttributeToSelect('*')->addUrlRewrite()->addStoreFilter($storeId);
 
         foreach ($products as $product) {
-            $productId      = $product->getId();
-            $productType    = $product->getTypeId();
-            $parentId = '';
+            $productId = $product->getId();
+            $productType = $product->getTypeId();
+
             if ($productType == "simple" || $productType == "virtual") {
-                $parentId = $this->getParentId($productId, $productType);
-            }
-            $productOptions = '';
-            if (in_array($productType, $productTypesWithOption)) {
-                $productOptions = $this->getProductOptions($product); // get product opitons for configurable/bundle/grouped product
+                if ($this->getParentId($productId, $productType)) {
+                    continue;
+                }
             }
 
-
-            if (!$parentId) {
-                $productsArray[] = [
-                    'categories' => $product->getCategoryIds(),
-                    'id'         => $productId,
-                    'sku'        => $product->getSku(),
-                    'imageUrl'   => (!empty($product->getImage())) ? $this->getProductImageUrl($product->getImage()) : '',
-                    'name'       => $product->getName(),
-                    'price'      => (!empty($product->getPrice())) ? $product->getPrice() : 0, // Does not return grouped/bundled parent price
-                    'url'        => $this->storeManager->getStore($storeId)->getBaseUrl() . $product->getRequestPath(),
-                    'options'    => $productOptions
-                ];
-            }
+            $productsArray[] = [
+                'categories' => $product->getCategoryIds(),
+                'id'         => $productId,
+                'sku'        => $product->getSku(),
+                'imageUrl'   => (!empty($product->getImage())) ? $this->getProductImageUrl($product->getImage()) : '',
+                'name'       => $product->getName(),
+                'price'      => (!empty($product->getPrice())) ? $product->getPrice() : 0, // Does not return grouped/bundled parent price
+                'url'        => $this->storeManager->getStore($storeId)->getBaseUrl() . $product->getRequestPath(),
+                'options'    => (in_array($productType, self::PARENT_TYPES)) ? $this->getProductOptions($product) : ''
+            ];
         }
-        
+
         return $productsArray;
     }
 
@@ -70,30 +68,23 @@ class ProductData
         foreach ($childrenProducts as $childProduct) {
             $productOptions[] = [
                 'productId' => $childProduct->getId(),
-                'sku'       => $childProduct->getSku(),
-                'name'      => $childProduct->getName(),
-                'price'     => $childProduct->getPrice(),
-                'imageUrl'  => $this->getProductImageUrl($childProduct->getImage())
+                'sku' => $childProduct->getSku(),
+                'name' => $childProduct->getName(),
+                'price' => $childProduct->getPrice(),
+                'imageUrl' => $this->getProductImageUrl($childProduct->getImage())
             ];
         }
 
         return $productOptions;
     }
 
-    protected function getProductImageUrl($imageUrlRequestPath) {
-
+    protected function getProductImageUrl($imageUrlRequestPath)
+    {
         return $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $imageUrlRequestPath;
     }
 
-    protected function getParentId($productId, $productType) {
-        $parentId = $this->groupedType->getParentIdsByChild($productId);
-        if (!$parentId) {
-            $parentId = $this->bundleType->getParentIdsByChild($productId);
-            if (!$parentId) {
-                $parentId = $this->configurableType->getParentIdsByChild($productId);
-            }
-        }
-
-        return $parentId;
+    protected function getParentId($productId, $productType)
+    {
+        return $this->configurableType->getParentIdsByChild($productId) || $this->bundleType->getParentIdsByChild($productId) || $this->groupedType->getParentIdsByChild($productId);
     }
 }
