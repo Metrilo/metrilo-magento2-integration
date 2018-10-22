99386417760d36d3dc0c41197e3b0f7523c2c8f6
Ned <ned@metrilo.com>
2018-10-22 14:28:42 +0300

    PLG-40: order data refactoring

--- Model/OrderData.php
+++ Model/OrderData.php
@@ -14,51 +14,55 @@ class OrderData
 
     public function getOrders($storeId)
     {
+        $ordersArray = [];
         $orders = $this->getOrderQuery($storeId);
 
         foreach ($orders as $order) {
-            if($order->getCustomerEmail()) {
-                $orderItems = $order->getAllItems(); // getAllVisibleItems() returns only parent products (for configurables) from order BUT ignoring child product quantities for BUNDLE products
-
-                foreach ($orderItems as $orderItem) {
-                    if ($orderItem->getProductType() != 'configurable' && $orderItem->getProductType() != 'bundle') { // exclude configurable/bundle parent product returned by getAllItems() method
-                        $orderProducts[] = [
-                            'productId' => $orderItem->getProductId(), // RETURNING PARENT PRODUCT ID IF CONFIGURABLE/BUNDLE PRODUCT
-                            'quantity'  => $orderItem->getQtyOrdered()
-                        ];
-                    }
-                }
-
-                $orderBillingData = $order->getBillingAddress();
-                $countryData      = $this->countryInterface->getCountryInfo($orderBillingData->getCountryId());
-                $street           = $orderBillingData->getStreet();
-                $couponCode       = $order->getCouponCode();
-
-                $orderBilling = [
-                    "firstName"     => $orderBillingData->getFirstname(),
-                    "lastName"      => $orderBillingData->getLastname(),
-                    "address"       => is_array($street) ? implode(PHP_EOL, $street) : $street,
-                    "city"          => $orderBillingData->getCity(),
-                    "country"       => $countryData->getFullNameEnglish(),
-                    "phone"         => $orderBillingData->getTelephone(),
-                    "postcode"      => $orderBillingData->getPostcode(),
-                    "paymentMethod" => $order->getPayment()->getMethodInstance()->getTitle()
-                ];
-
-                $ordersArray[] = [
-                    'id'        => $order->getId(),
-                    'createdAt' => strtotime($order->getCreatedAt()),
-                    'updatedAt' => strtotime($order->getUpdatedAt()),
-                    'email'     => $order->getCustomerEmail(),
-                    'amount'    => $order->getBaseGrandTotal(),
-                    'coupons'   => (!empty($couponCode)) ? $couponCode : '', // RETURNING NULL if there is no coupon applied // MULTYPLE coupon codes are available via extension ONLY
-                    'status'    => $order->getStatus(),
-                    'products'  => (isset($orderProducts)) ? $orderProducts : '',
-                    'billing'   => $orderBilling
-                ];
-
-                unset($orderProducts);
+            if(!$order->getCustomerEmail()) {
+                continue;
             }
+
+            $orderItems = $order->getAllItems(); // getAllVisibleItems() returns only parent products (for configurables) from order BUT ignoring child product quantities for BUNDLE products
+
+            foreach ($orderItems as $orderItem) {
+                $itemType = $orderItem->getProductType();
+                if ($itemType != 'configurable' && $itemType != 'bundle') { // exclude configurable/bundle parent product returned by getAllItems() method
+                    $orderProducts[] = [
+                        'productId' => $orderItem->getProductId(),
+                        'quantity'  => $orderItem->getQtyOrdered()
+                    ];
+                }
+            }
+
+            $orderBillingData = $order->getBillingAddress();
+            $countryData      = $this->countryInterface->getCountryInfo($orderBillingData->getCountryId());
+            $street           = $orderBillingData->getStreet();
+            $couponCode       = $order->getCouponCode();
+
+            $orderBilling = [
+                "firstName"     => $orderBillingData->getFirstname(),
+                "lastName"      => $orderBillingData->getLastname(),
+                "address"       => is_array($street) ? implode(PHP_EOL, $street) : $street,
+                "city"          => $orderBillingData->getCity(),
+                "country"       => $countryData->getFullNameEnglish(),
+                "phone"         => $orderBillingData->getTelephone(),
+                "postcode"      => $orderBillingData->getPostcode(),
+                "paymentMethod" => $order->getPayment()->getMethodInstance()->getTitle()
+            ];
+
+            $ordersArray[] = [
+                'id'        => $order->getIncrementId(),
+                'createdAt' => strtotime($order->getCreatedAt()),
+                'updatedAt' => strtotime($order->getUpdatedAt()),
+                'email'     => $order->getCustomerEmail(),
+                'amount'    => $order->getBaseGrandTotal(),
+                'coupons'   => (!empty($couponCode)) ? $couponCode : array(), // RETURNING NULL if there is no coupon applied // MULTYPLE coupon codes are available via extension ONLY
+                'status'    => $order->getStatus(),
+                'products'  => $orderProducts,
+                'billing'   => $orderBilling
+            ];
+
+            unset($orderProducts);
         }
 
         return $ordersArray;
