5944157418c4d26537314ab5a48ac3163a3d3857
nedick007 <40760980+nedick007@users.noreply.github.com>
2018-11-16 17:53:55 +0200

    PLG-40: [Import] Orders (#31)
    * PLG-40: OrderData class for import of order related data
    
    * PLG-40: code refactoring
    
    * PLG-40: order data refactoring
    
    * PLG-40: couponCode var from string to array plus code refactoring
    
    * PLG-40: replace country name with country id
    
    * PLG-40: refactor couponCode variable
    
    * PLG-40: couponCode refactoring

---
+++ Model/OrderData.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Metrilo\Analytics\Model;
+
+class OrderData
+{
+    public function __construct(
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollection
+    ) {
+        $this->orderCollection  = $orderCollection;
+    }
+
+    public function getOrders($storeId)
+    {
+        $ordersArray   = [];
+        $orders        = $this->getOrderQuery($storeId);
+
+        foreach ($orders as $order) {
+            if(!trim($order->getCustomerEmail())) {
+                continue;
+            }
+            $orderItems = $order->getAllItems();
+            $orderProducts = [];
+            
+            foreach ($orderItems as $orderItem) {
+                $itemType = $orderItem->getProductType();
+                if ($itemType == 'configurable' || $itemType == 'bundle') { // exclude configurable/bundle parent product returned by getAllItems() method
+                    continue;
+                }
+                $orderProducts[] = [
+                    'productId' => $orderItem->getProductId(),
+                    'quantity'  => $orderItem->getQtyOrdered()
+                ];
+            }
+
+            $orderBillingData = $order->getBillingAddress();
+            $street           = $orderBillingData->getStreet();
+            $couponCode       = $order->getCouponCode() ? [$order->getCouponCode()] : [];
+
+            $orderBilling = [
+                "firstName"     => $orderBillingData->getFirstname(),
+                "lastName"      => $orderBillingData->getLastname(),
+                "address"       => is_array($street) ? implode(PHP_EOL, $street) : $street,
+                "city"          => $orderBillingData->getCity(),
+                "country"       => $orderBillingData->getCountryId(),
+                "phone"         => $orderBillingData->getTelephone(),
+                "postcode"      => $orderBillingData->getPostcode(),
+                "paymentMethod" => $order->getPayment()->getMethodInstance()->getTitle()
+            ];
+
+            $ordersArray[] = [
+                'id'        => $order->getIncrementId(),
+                'createdAt' => strtotime($order->getCreatedAt()),
+                'updatedAt' => strtotime($order->getUpdatedAt()),
+                'email'     => $order->getCustomerEmail(),
+                'amount'    => $order->getBaseGrandTotal(),
+                'coupons'   => $couponCode,
+                'status'    => $order->getStatus(),
+                'products'  => $orderProducts,
+                'billing'   => $orderBilling
+            ];
+        }
+
+        return $ordersArray;
+    }
+
+    protected function getOrderQuery($storeId = 0)
+    {
+        return $this->orderCollection->create()->addAttributeToFilter('store_id', $storeId)->addAttributeToSelect('*')->setOrder('entity_id', 'asc');
+    }
+
+}
