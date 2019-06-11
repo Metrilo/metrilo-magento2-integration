1182024519f1be3009ca409c3db357854044b501
Ned <ned@metrilo.com>
2019-06-11 14:04:20 +0300

    PLG-113: removing debug code from constructor of category view event class

--- Model/Events/CategoryViewEvent.php
+++ Model/Events/CategoryViewEvent.php
@@ -8,7 +8,6 @@ class CategoryViewEvent
         \Magento\Framework\Registry $registry
     ) {
         $this->coreRegistry = $registry;
-        file_put_contents('CategoryViewEvent.txt', 'Category Event!', FILE_APPEND);
     }
     public function callJS() {
         return "window.metrilo.viewCategory('" . $this->coreRegistry->registry('current_category')->getId() . "');";
