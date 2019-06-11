e50154abc7d5b5eccadf53c72951d519e0fbbf37
Ned <ned@metrilo.com>
2019-06-11 18:26:04 +0300

    PLG-114: removing debug code from class constructor

--- Model/Events/CatalogSearch.php
+++ Model/Events/CatalogSearch.php
@@ -10,7 +10,6 @@ class CatalogSearch
     ) {
         $this->searchHelper = $searchHelper;
         $this->urlInterface = $urlInterface;
-        file_put_contents('CatalogView.txt', 'Catalog Event!' . PHP_EOL, FILE_APPEND);
     }
     public function callJS() {
         return "window.metrilo.search('" . $this->searchHelper->getEscapedQueryText() . "', '" . $this->urlInterface->getCurrentUrl() . "');";
