e90eeafe50ac6903cae6496f30590a43f4b2f42f
Ned <ned@metrilo.com>
2019-06-30 12:09:52 +0300

    PLG-18: Applying current customer group as tag param during customer events

--- Helper/CustomerSerializer.php
+++ Helper/CustomerSerializer.php
@@ -5,20 +5,25 @@ namespace Metrilo\Analytics\Helper;
 class CustomerSerializer extends \Magento\Framework\App\Helper\AbstractHelper
 {
     public function __construct(
-        \Magento\Newsletter\Model\Subscriber $subscriberModel
+        \Magento\Newsletter\Model\Subscriber           $subscriberModel,
+        \Magento\Customer\Api\GroupRepositoryInterface $groupRepository
     ) {
         $this->subscriberModel = $subscriberModel;
+        $this->groupRepository = $groupRepository;
     }
     
     public function serialize($customer) {
         $this->subscriberModel->unsetData();
+        $group       = $this->groupRepository->getById($customer->getGroupId());
+        $groupName[] = $group->getCode();
         
         $serializedCustomer = [
             'email'       => $customer->getEmail(),
             'createdAt'   => strtotime($customer->getCreatedAt()) * 1000,
             'firstName'   => $customer->getFirstname(),
             'lastName'    => $customer->getLastname(),
-            'subscribed'  => $this->subscriberModel->loadByCustomerId($customer->getId())->isSubscribed()
+            'subscribed'  => $this->subscriberModel->loadByCustomerId($customer->getId())->isSubscribed(),
+            'tags'        => $groupName
         ];
         
         return $serializedCustomer;
