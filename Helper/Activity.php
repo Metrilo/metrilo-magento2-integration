bbc3d6a5d8ad4d2bb516699050001ac9c6bed67c
Ned <ned@metrilo.com>
2019-07-03 14:56:24 +0300

    PLG-124: Removing unnecessary variable from activity helper

--- Helper/Activity.php
+++ Helper/Activity.php
@@ -4,18 +4,15 @@ namespace Metrilo\Analytics\Helper;
 
 class Activity extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    private $endPoint = 'http://p.metrilo.com';
-    
     public function createActivity($storeId, $type, $token, $secret)
     {
-        
         $data = array(
             'type'          => $type,
             'project_token' => $token,
             'signature'     => md5($token . $type . $secret)
         );
         
-        $url = $this->endPoint . '/tracking/' . $token . '/activity';
+        $url = 'http://p.metrilo.com/tracking/' . $token . '/activity';
         
         return array('url' => $url, 'data' => $data);
     }
