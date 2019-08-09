cb667cd3bef596ccc46126ff8c2bcb75c2afc155
Ned <ned@metrilo.com>
2019-08-09 17:48:12 +0300

    PLG-119: FPC hole punching for customer specific events like add/remove from cart and identify

---
+++ view/frontend/web/js/private-data.js
@@ -0,0 +1,23 @@
+define([
+    'uiComponent',
+    'Magento_Customer/js/customer-data',
+    'jquery'
+], function (Component, customerData, $) {
+    'use strict';
+
+    return Component.extend({
+        /** @inheritdoc */
+        initialize: function () {
+            this._super();
+            this.metrilosection = customerData.get('metrilo-private-data');
+
+            if (this.metrilosection) {
+                this.metrilosection.subscribe(function () {
+                    $.each(this.metrilosection().metrilo_events, function(key, val){
+                        eval(val.replace("window.", ""));
+                    });
+                }, this);
+            }
+        }
+    });
+});
