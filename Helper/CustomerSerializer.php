d139ecfb6eea22ee0fc8c1d4f200512e7873bfb7
nedick007 <40760980+nedick007@users.noreply.github.com>
2020-01-10 16:14:39 +0200

    KYP-672: Added customer event for order creation during guest checkout and code refactoring for the customer observer class (#70)

--- Helper/CustomerSerializer.php
+++ Helper/CustomerSerializer.php
@@ -4,26 +4,14 @@ namespace Metrilo\Analytics\Helper;
 
 class CustomerSerializer extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    public function __construct(
-        \Magento\Newsletter\Model\Subscriber           $subscriberModel,
-        \Magento\Customer\Api\GroupRepositoryInterface $groupRepository
-    ) {
-        $this->subscriberModel = $subscriberModel;
-        $this->groupRepository = $groupRepository;
-    }
-    
     public function serialize($customer) {
-        $this->subscriberModel->unsetData();
-        $group       = $this->groupRepository->getById($customer->getGroupId());
-        $groupName[] = $group->getCode();
-        
         $serializedCustomer = [
             'email'       => $customer->getEmail(),
-            'createdAt'   => strtotime($customer->getCreatedAt()) * 1000,
-            'firstName'   => $customer->getFirstname(),
-            'lastName'    => $customer->getLastname(),
-            'subscribed'  => $this->subscriberModel->loadByCustomerId($customer->getId())->isSubscribed(),
-            'tags'        => $groupName
+            'createdAt'   => $customer->getCreatedAt(),
+            'firstName'   => $customer->getFirstName(),
+            'lastName'    => $customer->getLastName(),
+            'subscribed'  => $customer->getSubscriberStatus(),
+            'tags'        => $customer->getTags()
         ];
         
         return $serializedCustomer;
