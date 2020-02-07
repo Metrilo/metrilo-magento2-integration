cbef3698aaaff12246a94fb7d8cfa467e09d2ca3
Ned <ned@metrilo.com>
2020-02-07 15:52:07 +0200

    KYP-761: code refactoring

--- Observer/AddToCart.php
+++ Observer/AddToCart.php
@@ -70,7 +70,8 @@ class AddToCart implements ObserverInterface
         if ($product->getId() != $childProduct->getId()) {
             $product = $this->productRepository->getById($product->getId());
             // for legacy reasons - we have been passing the SKU as ID for the child products
-            $data['option_id'] = $childProduct->getSku() ? $childProduct->getSku() : $childProduct->getId();
+            $optionSku = $childProduct->getSku();
+            $data['option_id'] = $optionSku ? $optionSku : $childProduct->getId();
             $data['option_sku'] = $childProduct->getSku();
             $data['option_name'] = $childProduct->getName();
             $data['option_price'] = (float)$childProduct->getFinalPrice();
