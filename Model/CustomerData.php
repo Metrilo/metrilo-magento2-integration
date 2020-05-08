52cf53b1441ee898b627516fab9948a5edff70c4
Ned <ned@metrilo.com>
2020-05-08 16:37:31 +0300

    KYP-1058: Removing debug code

--- Model/CustomerData.php
+++ Model/CustomerData.php
@@ -1,20 +1,44 @@
 <?php
 
 namespace Metrilo\Analytics\Model;
+use Metrilo\Analytics\Helper\MetriloCustomer;
 
 class CustomerData
 {
+    private $customerCollection;
+    private $subscriberModel;
+    private $groupRepository;
+    
     public $chunkItems = \Metrilo\Analytics\Helper\Data::chunkItems;
 
     public function __construct(
-        \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollection
+        \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollection,
+        \Magento\Newsletter\Model\Subscriber                             $subscriberModel,
+        \Magento\Customer\Api\GroupRepositoryInterface                   $groupRepository
     ) {
         $this->customerCollection = $customerCollection;
+        $this->subscriberModel    = $subscriberModel;
+        $this->groupRepository   = $groupRepository;
     }
 
     public function getCustomers($storeId, $chunkId)
     {
-        return $this->getCustomerQuery($storeId)->setPageSize($this->chunkItems)->setCurPage($chunkId + 1);
+        $metriloCustomers = [];
+        $customers  = $this->getCustomerQuery($storeId)->setPageSize($this->chunkItems)->setCurPage($chunkId + 1);
+        
+        foreach ($customers as $customer) {
+            $metriloCustomers[] = new MetriloCustomer(
+                $customer->getStoreId(),
+                $customer->getEmail(),
+                strtotime($customer->getCreatedAt()) * 1000,
+                $customer->getData('firstname'),
+                $customer->getData('lastname'),
+                $this->getCustomerSubscriberStatus($customer->getId()),
+                $this->getCustomerGroup($customer->getGroupId())
+            );
+        }
+        
+        return $metriloCustomers;
     }
 
     public function getCustomerChunks($storeId)
@@ -26,5 +50,16 @@ class CustomerData
     private function getCustomerQuery($storeId)
     {
         return $this->customerCollection->create()->addAttributeToFilter('store_id', $storeId);
+    }
+
+    private function getCustomerSubscriberStatus($customerId) {
+        $this->subscriberModel->unsetData();
+        return $this->subscriberModel->loadByCustomerId($customerId)->isSubscribed();
+    }
+
+    private function getCustomerGroup($groupId) {
+        $group       = $this->groupRepository->getById($groupId);
+        $groupName[] = $group->getCode();
+        return $groupName;
     }
 }
