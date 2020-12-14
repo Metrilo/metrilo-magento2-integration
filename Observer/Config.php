8ca0f1f9c01d3f6f16b3de15d73cbf41516eae1e
nedick007 <40760980+nedick007@users.noreply.github.com>
2020-12-14 15:15:05 +0200

    Kyp 1569/magento install error when adding api to the default store view (#96)
    * KYP-1569: Updated admin message for default configuration scope settings save
    
    * KYP-1569: Cast observer store id value to int because it might return a string value

--- Observer/Config.php
+++ Observer/Config.php
@@ -20,11 +20,22 @@ class Config implements ObserverInterface
     public function execute(Observer $observer)
     {
         try {
-            if (!$this->activityHelper->createActivity($observer->getStore(), 'integrated')) {
-                $this->messageManager->addError(
-                    'The API Token and/or API Secret you have entered are invalid.
+            $storeId = $observer->getStore();
+            if (!$this->activityHelper->createActivity($storeId, 'integrated')) {
+                if ((int)$storeId === 0) {
+                    $this->messageManager->addError(
+                        'You\'ve just entered the API token and API Secret to the default configuration scope.
+                        This means that the Metrilo module will be added to all your store views.
+                        If you want to connect only a specific store view, please remove it form the default scope and
+                        add it only to the specific store view configuration scope.
+                        You can find the "Import" button by opening any specific configuration scope.'
+                    );
+                } else {
+                    $this->messageManager->addError(
+                        'The API Token and/or API Secret you have entered are invalid.
                     You can find the correct ones in Settings -> Installation in your Metrilo account.'
-                );
+                    );
+                }
             }
         } catch (\Exception $e) {
             $this->dataHelper->logError($e);
